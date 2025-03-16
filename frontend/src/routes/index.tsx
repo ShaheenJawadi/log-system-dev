b5e84ca1d67565b2b131d7846fb166a9072a7601
shaheenj <jawadishaheen@gmail.com>
2025-03-16 04:09:43 +0100

    Refactor frontend structure - add routing

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { RouteObject } from "react-router-dom";
+import Home from "../pages/home";
+ 
+
+export const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <Home/>,  
+  } 
+  
+];
