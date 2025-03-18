e29a8ae030c614d9812bb7bf7be27dbf8ba14bdd
shaheenj <jawadishaheen@gmail.com>
2025-03-18 08:04:30 +0100

    Refactor NewTrip component to adjust map height dynamically based on header height

--- frontend/src/pages/newTrip.tsx
+++ frontend/src/pages/newTrip.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Box } from "@mui/material";
 import { MapContainer, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css"; 
-import TripDetails from "../components/newTrip/tripDetails";
 
+import TripDetails from "../components/newTrip/tripDetails";
+import "leaflet/dist/leaflet.css";
+import { headerHeight } from "../utils/constatnts";
 const NewTrip: React.FC = () => {
   return (
     <Box sx={{ height: "100%", position: "relative" }}>
-   
-        <TripDetails /> 
+      <TripDetails />
       <MapContainer
         center={[51.505, -0.09]}
         zoom={100}
         scrollWheelZoom={true}
-        style={{ width: "100%", height: "100%" }}
+        style={{ width: "100%", height: `calc(100vh - ${headerHeight}px)` , flex: 1}}  
       >
         <TileLayer
           attribution=' <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -23,6 +23,5 @@ const NewTrip: React.FC = () => {
     </Box>
   );
 };
-
 
 export default NewTrip;
