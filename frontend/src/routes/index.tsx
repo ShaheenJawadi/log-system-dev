b5e84e01dace7ddfa14e46575f78f5e4d6cc6a3a
shaheenj <jawadishaheen@gmail.com>
2025-03-19 11:52:20 +0100

    Add login and registration pages with routing

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,9 +3,22 @@ import Home from "../pages/home";
 import MainPageContainer from "../components/mainPagesContainer";
 import NewTrip from "../pages/newTrip";
 import DriverLog from "../pages/driverlog";
+import LoginPage from "../pages/login";
+import RegisterPage from "../pages/register";
  
 
 export const routes: RouteObject[] = [
+
+  {
+    path: "/login",
+    element:<LoginPage/>,  
+  } 
+
+  ,
+  {
+    path: "/register",
+    element:<RegisterPage/>,  
+  } ,
   {
     path: "/",
     element:    <MainPageContainer><Home/></MainPageContainer>,  
@@ -19,4 +32,8 @@ export const routes: RouteObject[] = [
     path: "/my-logs",
     element:<MainPageContainer><DriverLog/></MainPageContainer>,  
   } 
+
+
+  ,
+
 ];
