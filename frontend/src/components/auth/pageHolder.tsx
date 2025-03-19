cb0e5d1e7983cef8b7d8b13589f879fae18ed663
shaheenj <jawadishaheen@gmail.com>
2025-03-19 12:28:39 +0100

    Enhance registration page

--- frontend/src/components/auth/pageHolder.tsx
+++ frontend/src/components/auth/pageHolder.tsx
@@ -10,44 +10,53 @@ import {
 } from "@mui/material";
 import { ReactNode } from "react";
 import Logo from "../Logo";
-import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import PerfectScrollbar from "react-perfect-scrollbar";
 
 const Holder = styled(Box)(({ theme }) => ({
   color: theme.palette.primary.contrastText,
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
-  height: "100vh",
+  minHeight: "100vh",
   width: "100vw",
+  margin: 5,
 }));
 
-const AuthPageHolder = ({ children , title  }: { children: ReactNode , title : string }) => {
+const AuthPageHolder = ({
+  children,
+  title,
+}: {
+  children: ReactNode;
+  title: string;
+}) => {
   return (
-    <Holder>
-      <Card>
-        <CardContent>
-          <Stack paddingX={5} spacing={3}>
-            <Stack alignItems={"center"} direction={"row"}>
-              <Logo size={50} />
-              <CardHeader
-                title="Truckers Journal"
-                subheader="Your personal trucking assistant"
-              />
-            </Stack>
-            <Divider />
-            <Stack alignItems={"center"} spacing={4} direction={"row"}>
-             
-              <Typography fontWeight={600} fontSize={23} color="secondary">
-                {title}
-              </Typography>
-              <LocalShippingIcon fontSize={"medium"} color="secondary" />
-            </Stack>
+    <PerfectScrollbar>
+      <Holder>
+        <Card>
+          <CardContent>
+            <Stack paddingX={5} spacing={3}>
+              <Stack alignItems={"center"} direction={"row"}>
+                <Logo size={50} />
+                <CardHeader
+                  title="Truckers Journal"
+                  subheader="Your personal trucking assistant"
+                />
+              </Stack>
+              <Divider />
+              <Stack alignItems={"center"} spacing={4} direction={"row"}>
+                <Typography fontWeight={600} fontSize={23} color="secondary">
+                  {title}
+                </Typography>
+                <LocalShippingIcon fontSize={"medium"} color="secondary" />
+              </Stack>
 
-            {children}
-          </Stack>
-        </CardContent>
-      </Card>
-    </Holder>
+              {children}
+            </Stack>
+          </CardContent>
+        </Card>
+      </Holder>
+    </PerfectScrollbar>
   );
 };
 
--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,11 +1,88 @@
+import { VisibilityOff, Visibility } from "@mui/icons-material";
+import {
+  Button,
+  Divider,
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Stack,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
 
 const RegisterPage = () => {
- 
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
 
   return (
-    <>
-   
-    </>
+    <Stack spacing={4}>
+       <FormControl variant="outlined">
+        <InputLabel>First name</InputLabel>
+        <OutlinedInput type={"text"} label="Username" />
+      </FormControl>
+      <FormControl variant="outlined">
+        <InputLabel>Last name</InputLabel>
+        <OutlinedInput type={"text"} label="Username" />
+      </FormControl>
+      <FormControl variant="outlined">
+        <InputLabel>Email</InputLabel>
+        <OutlinedInput type={"text"} label="Username" />
+      </FormControl>
+      <FormControl variant="outlined">
+        <InputLabel>Username</InputLabel>
+        <OutlinedInput type={"text"} label="Username" />
+      </FormControl>
+      <FormControl variant="outlined">
+        <InputLabel>Password</InputLabel>
+        <OutlinedInput
+          type={showPassword ? "text" : "password"}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label={
+                  showPassword ? "hide the password" : "display the password"
+                }
+                onClick={handleClickShowPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+      </FormControl>
+
+      <FormControl variant="outlined">
+        <InputLabel>Repeat Password</InputLabel>
+        <OutlinedInput
+          type={showPassword ? "text" : "password"}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label={
+                  showPassword ? "hide the password" : "display the password"
+                }
+                onClick={handleClickShowPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+      </FormControl>
+
+      <Button variant="contained">Register</Button>
+
+      <Divider />
+      <Typography variant="caption">You   have an accout ?</Typography>
+      <Button variant="outlined">Login here</Button>
+    </Stack>
   );
 };
 
