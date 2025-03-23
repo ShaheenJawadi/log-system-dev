ed05c83d495b33c0824d264eebe76c792a5c6794
shaheenj <jawadishaheen@gmail.com>
2025-03-23 10:05:51 +0100

    Add LogSheet type to define structure for log entries and related metrics

--- frontend/src/types/logs.ts
+++ frontend/src/types/logs.ts
@@ -15,3 +15,27 @@ export type LogDay = {
   date: string;
   entries: LogEntry[];
 };
+
+
+type LogSheet = {
+  log_days: LogDay[];  
+
+  totalMilesToday?: number | null;
+  totalMileageToday?: number | null;
+  truckInfo?: string | null;
+  carrierName?: string | null;
+  officeAddress?: string | null;
+  terminalAddress?: string | null;
+  manifestNumber?: string | null;
+  shipperCommodity?: string | null;
+  date?: string | null; // ISO date string
+  fromLocation?: string | null;
+  toLocation?: string | null;
+  OnDutyHoursToday?: number | null;
+  totalOnDutyLast7Days?: number | null;
+  totalAvailableTomorrow70?: number | null;
+  totalOnDutyLast5Days?: number | null;
+  totalOnDutyLast8Days?: number | null;
+  totalAvailableTomorrow60?: number | null;
+  totalOnDutyLast7Days60?: number | null;
+};
