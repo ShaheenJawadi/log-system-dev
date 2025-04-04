30ddf896a4f811cea78b5d0b39f92b6ef489c305
shaheenj <jawadishaheen@gmail.com>
2025-04-04 07:04:13 +0100

    Refactor MainHeader to include back navigation and improve layout

--- frontend/src/components/mainHeader.tsx
+++ frontend/src/components/mainHeader.tsx
@@ -1,18 +1,15 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, IconButton, Stack, Typography } from "@mui/material";
 import React from "react";
 import Logo from "./Logo";
 import { headerHeight } from "../utils/constatnts";
 import HomeIcon from "@mui/icons-material/Home";
 import { useNavigate } from "react-router-dom";
 import { appPaths } from "../routes/paths";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 const MainHeader: React.FC<{ title: string }> = ({ title }) => {
-
   const navigate = useNavigate();
 
   return (
-
-
-
     <div>
       <Box sx={{ backgroundColor: "secondary.main" }}>
         <Stack
@@ -35,21 +32,34 @@ const MainHeader: React.FC<{ title: string }> = ({ title }) => {
             </Typography>
           </Box>
           <Stack
-          onClick={() =>navigate(appPaths.home) }
-            spacing={3}
-            alignItems={"center"}
             direction={"row"}
-            justifyContent={"center"}
             sx={{
-              backgroundColor: "primary.main",
               height: "100%",
-              paddingX: 5,
             }}
+            alignItems={"center"}
           >
-            <HomeIcon sx={{ color: "#fff", fontSize: 30 }} />
-            <Typography variant="h6" fontWeight={600} color="white">
-              Home
-            </Typography>
+            <Box>
+              <IconButton sx={{ color: "#fff" }} onClick={() => navigate(-1)}>
+                <ArrowBackIosIcon />
+              </IconButton>
+            </Box>
+            <Stack
+              onClick={() => navigate(appPaths.home)}
+              spacing={3}
+              alignItems={"center"}
+              direction={"row"}
+              justifyContent={"center"}
+              sx={{
+                height: "100%",
+                backgroundColor: "primary.main",
+                paddingX: 5,
+              }}
+            >
+              <HomeIcon sx={{ color: "#fff", fontSize: 30 }} />
+              <Typography variant="h6" fontWeight={600} color="white">
+                Home
+              </Typography>
+            </Stack>{" "}
           </Stack>
         </Stack>
       </Box>
